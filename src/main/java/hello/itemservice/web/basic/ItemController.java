740312d96ad65cd24f08a176bf3127694c18ca03
alenjb <alenjb@naver.com>
2024-01-22 02:00:54 +0900

    refactor: 상품 등록 중복 방지 구현

--- src/main/java/hello/itemservice/web/basic/ItemController.java
+++ src/main/java/hello/itemservice/web/basic/ItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -38,25 +39,27 @@ public class ItemController {
     }
 
 //    @PostMapping("/add")
-    public String save(@RequestParam(name = "itemName") String itemName,
-                       @RequestParam(name = "price") int price,
-                       @RequestParam(name = "quantity")Integer quantity,
-                       Model model){
-        Item item = new Item();
-        item.setItemName(itemName);
-        item.setPrice(price);
-        item.setQuantity(quantity);
-
-        itemRepository.save(item);
-
-        model.addAttribute("item", item);
-        return "basic/item";
-    }
+//    public String save(@RequestParam(name = "itemName") String itemName,
+//                       @RequestParam(name = "price") int price,
+//                       @RequestParam(name = "quantity")Integer quantity,
+//                       Model model){
+//        Item item = new Item();
+//        item.setItemName(itemName);
+//        item.setPrice(price);
+//        item.setQuantity(quantity);
+//
+//        itemRepository.save(item);
+//
+//        model.addAttribute("item", item);
+//        return "basic/item";
+//    }
 
     @PostMapping("/add")
-    public String save(@ModelAttribute("item") Item item){
-        itemRepository.save(item);
-        return "basic/item";
+    public String save(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
