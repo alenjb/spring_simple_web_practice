7d8a1283c0662047d8a577559296550dbb6803ec
alenjb <alenjb@naver.com>
2024-01-22 01:25:45 +0900

    feat: 상품 등록 기능 구현

--- src/main/java/hello/itemservice/web/basic/ItemController.java
+++ src/main/java/hello/itemservice/web/basic/ItemController.java
@@ -7,9 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +29,33 @@ public class ItemController {
     public String item(@PathVariable(name = "itemId") long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String save(@RequestParam(name = "itemName") String itemName,
+                       @RequestParam(name = "price") int price,
+                       @RequestParam(name = "quantity")Integer quantity,
+                       Model model){
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String save(@ModelAttribute("item") Item item){
+        itemRepository.save(item);
         return "basic/item";
     }
 
